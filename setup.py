d0914e97e036ea6382ba818f2b26402e29991383
Andrea Micheli <amicheli@fbk.eu>
2022-04-20 14:29:56 +0200

    Modified setup.py to signal that the generated wheel is platform-specific, but not linked to the python ABI. This should allow to build wheels for the different OSs easily

--- setup.py
+++ setup.py
@@ -9,6 +9,25 @@ import urllib
 import shutil
 import sys
 
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
+    class bdist_wheel(_bdist_wheel):
+
+        def finalize_options(self):
+            _bdist_wheel.finalize_options(self)
+            # Mark us as not a pure python package
+            self.root_is_pure = False
+
+        def get_tag(self):
+            python, abi, plat = _bdist_wheel.get_tag(self)
+            # We don't link with python ABI, but require python3
+            python, abi = 'py3', 'none'
+            return python, abi, plat
+except ImportError:
+    bdist_wheel = None
+
+
 
 FAST_DOWNWARD_REPO = 'https://github.com/aibasel/downward.git'
 FAST_DOWNWARD_RELEASE = 'release-21.12'
@@ -58,6 +77,7 @@ setup(name='up_fast_downward',
               'downward/driver/*', 'downward/driver/portfolios/*']
       },
       cmdclass={
+          'bdist_wheel': bdist_wheel,
           'build_py': install_fast_downward,
           'develop': install_fast_downward_develop,
       })
