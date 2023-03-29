753e34bf8a06def5c1231fc34c07e7148c9ab8c6
Luca Framba <framba@fbk.eu>
2023-03-29 17:16:56 +0200

    Added non-constant boolean assignments flag to the Reachability Grounder

--- up_fast_downward/fast_downward_grounder.py
+++ up_fast_downward/fast_downward_grounder.py
@@ -59,6 +59,8 @@ class FastDownwardReachabilityGrounder(Engine, CompilerMixin):
         supported_kind.set_conditions_kind("DISJUNCTIVE_CONDITIONS")
         supported_kind.set_conditions_kind("EQUALITIES")
         supported_kind.set_effects_kind("CONDITIONAL_EFFECTS")
+        supported_kind.set_effects_kind("STATIC_FLUENTS_IN_BOOLEAN_ASSIGNMENTS")
+        supported_kind.set_effects_kind("FLUENTS_IN_BOOLEAN_ASSIGNMENTS")
         supported_kind.set_quality_metrics("ACTIONS_COST")
         supported_kind.set_quality_metrics("PLAN_LENGTH")
         supported_kind.set_conditions_kind("UNIVERSAL_CONDITIONS")
